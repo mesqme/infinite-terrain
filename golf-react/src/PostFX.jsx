ed03d1f3697772006741230d890b7930ffc3d0d6
mesqme <kiyatkin.misha@gmail.com>
2025-12-06 03:20:34 +0500

    feat: remove ao postfx

--- golf-react/src/PostFX.jsx
+++ golf-react/src/PostFX.jsx
@@ -1,23 +1,16 @@
 import {
-	EffectComposer,
-	Bloom,
-	N8AO,
-	ToneMapping,
+    EffectComposer,
+    Bloom,
+    N8AO,
+    ToneMapping,
 } from '@react-three/postprocessing'
 import { ToneMappingMode } from 'postprocessing'
 
 export default function PostFX() {
-	return (
-		<EffectComposer>
-			<N8AO
-				aoRadius={66}
-				distanceFalloff={0.21}
-				intensity={0.5}
-				screenSpaceRadius={true}
-				halfRes={false}
-			/>
-			{/* <Bloom /> */}
-			<ToneMapping mode={ToneMappingMode.ACES_FILMIC} />
-		</EffectComposer>
-	)
+    return (
+        <EffectComposer>
+            {/* <Bloom /> */}
+            <ToneMapping mode={ToneMappingMode.ACES_FILMIC} />
+        </EffectComposer>
+    )
 }
